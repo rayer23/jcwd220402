3ffd273ad870610be2862246bf6ade82dadc99b5
rayer23 <fadelfarhan27@gmail.com>
2022-12-23 14:44:20 +0700

    u

--- projects/server/config/config.js
+++ projects/server/config/config.js
@@ -3,7 +3,7 @@ module.exports = {
   development: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: "ecommerce_warehouse",
+    database: "delisha",
     host: "127.0.0.1",
     dialect: "mysql",
   },
